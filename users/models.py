e524e17c79427cfcf36db172f680181bdc3a8051
PetrDidur <pdidur@yandex.ru>
2023-11-15 11:45:29 +0300

    first commit

---
+++ users/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='электронная почта')
+    phone = models.CharField(max_length=50, verbose_name='номер телефона', **NULLABLE)
+    city = models.CharField(max_length=150, verbose_name='город', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+
+
